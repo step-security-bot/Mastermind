47370dd058e9ff40023980fbf12c81d3b1119219
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2024-11-07 17:12:21 -0800

    feat: implement AISetter and ExternalSetter class

--- players.py
+++ players.py
@@ -3,6 +3,7 @@ from .validation import BaseModel, ValidGuess, ValidFeedback
 from .utils import FStringTemplate, get_feedback
 from abc import ABC, abstractmethod
 from getpass import getpass
+from random import randint
 
 
 # Abstract Class for Player Unit
@@ -83,11 +84,6 @@ class HumanSetter(CodeSetter):
     """
     A class to represent a human code setter.
     """
-    def __init__(self, game: Game) -> None:
-        """
-        Initializes the human code setter.
-        """
-        super().__init__(game)
 
     def set_secret_code(self) -> None:
         """
@@ -116,7 +112,6 @@ class HumanSetter(CodeSetter):
                     continue
                 self.SECRET_CODE = valid_guess
                 break
-
     
     def get_feedback(self, guess: tuple) -> tuple:
         """
@@ -157,11 +152,74 @@ class HumanCracker(CodeCracker):
             
             try:
                 valid_guess.value = valid_guess.validate(guess)
+                break
             except ValueError as e:
                 print(e)
                 print("To get more help, enter '?' for tips.")
-            else:
-                break
         return valid_guess
 
 
+class AISetter(CodeSetter):
+    """
+    A class to represent an AI code setter.
+    """
+    
+    def set_secret_code(self) -> None:
+        """
+        Sets the secret code for the game.
+        """
+        # Generate random code
+        number_of_colors = self.GAME.number_of_colors
+        number_of_dots = self.GAME.number_of_dots
+        self.SECRET_CODE = tuple(randint(1, number_of_colors) for _ in range(number_of_dots))
+    
+    def get_feedback(self, guess: tuple) -> tuple:
+        """
+        Obtains feedback for a given guess.
+        """
+        if not hasattr(self, 'SECRET_CODE'):
+            raise NotImplementedError("Secret code not set yet.")
+        return get_feedback(guess, self.SECRET_CODE)
+
+
+class ExternalSetter(CodeSetter):
+    """
+    A class to represent an external code setter.
+    """
+    
+    def set_secret_code(self) -> None:
+        """
+        Sets the secret code for the game.
+        """
+        pass  # There is no code available for external game, skip it
+    
+    def get_feedback(self, guess: tuple) -> tuple:
+        """
+        Obtains external feedback from the user.
+        """
+        valid_feedback = ValidFeedback((0,0), number_of_dots=self.GAME.number_of_dots)
+        while True:
+            feedback = input("Enter the feedback: ")
+            if feedback == "?":
+                hint = f"""
+                Enter a 2 digit number (optionally separated by comma) between 0 and {self.GAME.number_of_dots}.
+                The first digit represents the number of black pegs, the second represents the number of white pegs.
+                For example: 01 or 0,1 -> (0, 1) -> 0 black pegs, 1 white peg
+                """
+                print(hint)
+                continue
+            
+            try:
+                valid_feedback.value = valid_feedback.validate(feedback)
+                break
+            except ValueError as e:
+                print(e)
+                print("To get more help, enter '?' for tips.")
+        return valid_feedback
+
+
+class AICracker(CodeCracker):
+    """
+    A class to represent an AI code cracker.
+    """
+    pass  # TODO: Implement solver logic.
