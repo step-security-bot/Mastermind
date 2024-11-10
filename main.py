8b9f40562c689c8bbaed0aac3cae928153600bca
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2024-11-09 22:22:43 -0800

    feat: complete MainUI logic; add GameHandler; rename save_data method

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
-
-class UserProfile:
-    """An utility class to store and access user profile data."""
-
-    @classmethod
-    def create_new_user(cls):
-        pass
+from .storage_handler import UserData
 
 
 class UserSettings:
@@ -80,45 +74,90 @@ class UserMenus:
 class GameStatistics:
     """An utility class to compute and display game statistics."""
 
-class GameSimulator:
+
+class GameHandler:
+    """Communication between MainUI and the gameboard."""
+
+    def start_new_game(cls, game_mode: str) -> None:
+        """Start a new game."""
+        if game_mode == "HvH":
+            pass
+        elif game_mode == "HvAI":
+            pass
+        elif game_mode == "AIvH":
+            pass
+        elif game_mode == "AIvAI":
+            pass
+        else:
+            raise Exception("Unexpected invalid game mode.")
+
+
+class MainUI:
+    """Class to handle the user menu interface."""
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(CrawlerSingleton, cls).__new__(cls)
         return cls.instance
 
-    def main_menu(self):
-        """Display the main menu and handle user input."""
+    def main_menu(self) -> bool:
+        """
+        Display the main menu and handle user input.
+        Return whether the user want to exit.
+        """
         choice = UserMenus.MainMenu()(5)
 
         if choice == "Start New Game":
-            self.start_new_game()
+            self.new_game_menu()
+            return True
+        
         elif choice == "Load Saved Game":
-            self.load_saved_game()
+            self.saved_game_menu()
+            return True
+        
         elif choice == "My Statistics":
-            self.show_statistics()
+            raise NotImplementedError("Statistics not implemented yet.")
+            return True
+        
         elif choice == "Settings":
-            self.settings()
+            raise NotImplementedError("Settings not implemented yet.")
+            return True
+        
         elif choice == "Save and Exit":
-            self.save_and_exit()
+            return False  # terminate the loop
 
-    def new_game(self):
+    def new_game_menu(self):
         """Display the new game menu and handle user input."""
         choice = UserMenus.NewGameMenu()(5)
 
         if choice == "You vs Someone Else":
-            pass
+            GameHandler.start_new_game("HvH")
+            return True
         elif choice == "You vs AI":
-            pass
+            GameHandler.start_new_game("HvAI")
+            return True
         elif choice == "AI vs You":
-            pass
+            GameHandler.start_new_game("AIvH")
+            return True
         elif choice == "Solve External Game":
-            pass
+            GameHandler.start_new_game("AIvAI")
+            return True
         elif choice == "Return to Main Menu":
-            self.main_menu()
+            return False  # terminate the loop
+        else:
+            raise Exception("Unexpected invalid choice.")
+        
+    def saved_game_menu(self):
+        """Display the saved game menu and handle user input."""
+        pass
 
     def run(self):
-        pass
+        """Run the game."""
+        print("Welcome to Mastermind!")
+        while self.main_menu():
+            pass  # keep calling self.main_menu() untill it return False
+        print("Thank you for playing!")
+        UserData.save_data()
 
 
 if __name__ == "__main__":
