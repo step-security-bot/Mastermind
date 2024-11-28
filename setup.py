5f29ccbdaa32ea5951af6e69c3e85f99c05b5cbd
FlysonBot <flysonlin2@gmail.com>
2024-11-28 05:10:12 +0000

    fix: Project requires python v3.9 rather than v3.8 becuase pandas requires v3.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(where="src"),
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Education",
@@ -27,7 +27,7 @@ setup(
         "Topic :: Games/Entertainment :: Puzzle Games",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=read_requirements("requirements.txt"),
     entry_points={
         "console_scripts": [
